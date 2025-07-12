017745e4414e0b8ee40d8c293afb4dcc19d996c5
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-07-11 21:27:11 -0300

    style: estilizando a home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,17 @@
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import * as S from './style'
+
 export const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
+        <main>
+            <S.Container>
+                <h1><FaGithub size={25} /> Meus repositórios</h1>
+
+                <S.Form>
+                    <input type="text" placeholder="Adicionar Repositórios" />
+                    <S.Button type="submit"><FaPlus color='#fff' size={14} /></S.Button>
+                </S.Form>
+            </S.Container>
+        </main>
+    )
 }
--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -1,2 +1,44 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    max-width: 700px;
+    background-color: #fff;
+    padding: 30px;
+    margin: 80px auto;
+    border-radius: 4px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+
+    h1 {
+        font-size: 20px;
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-right: 10px;
+        }
+    }
+`
+
+export const Form = styled.form`
+    margin-top: 30px;
+    display: flex;
+
+    input {
+        flex: 1;
+        border: 1px solid #DDD;
+        padding: 10px 15px;
+        border-radius: 4px;
+        font-size: 17px;
+    }
+`
+
+export const Button = styled.button`
+    background-color: #0d2636;
+    border: none;
+    border-radius: 4px;
+    margin-left:  10px;
+    padding: 0 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
